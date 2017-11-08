05a022e1d340a5fdc7f05a68770d7e410cd58398
Luke Pritchard <luke-pritch@users.noreply.github.com>
2017-11-08 16:13:02 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.  It's intended to be used at an MLH Localhost
+ * Workshop.
+ *
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/mlh/mlh-localhost-hacking-with-alexa
+ **/
+
+'use strict';
+
+// TODO: replace with facts about yourself
+const FACTS = [
+	"Luke is a really cool dude.",
+	"The American Revolution Happened in 1776!",
+	"Phase One is a great club",
+	"The Western Roman Empire collapsed on 476 AD."
+];
+
+var handlers = {
+  'LaunchRequest': function () { this.emit('GetFact'); },
+  'GetNewFactIntent': function () { this.emit('GetFact'); },
+  'GetFact': function() {
+    // Randomly select a fact from the array
+    const factIndex = Math.floor(Math.random() * FACTS.length);
+    const randomFact = FACTS[factIndex];
+
+    // Create speech output
+    const speechOutput = "Here's your fact: " + randomFact;
+    this.emit(':tellWithCard', speechOutput, "Major League Hacking (MLH) Facts", randomFact);
+  }
+};
+
+// This is the function that AWS Lambda calls every time Alexa uses your skill.
+exports.handler = function(event, context, callback) {
+  // Include the AWS Alexa Library.
+  const Alexa = require("alexa-sdk");
+
+  // Create an instance of the Alexa library and pass it the requested command.
+  var alexa = Alexa.handler(event, context);
+
+  // Give our Alexa instance instructions for handling commands and execute the request.
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
